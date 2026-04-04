609f76a9e8980d4cc9e789156ed5a064c656962d
havanthe2004 <hathe242424+new@gmail.com>
2026-04-04 17:25:28 +0700

    Xoá

--- backend/src/config/uploaders.ts
+++
@@ -1,5 +0,0 @@
-import { createUploader } from "./multer";
-
-export const uploadUserAvatar = createUploader("avatars/user");
-export const uploadAdminAvatar = createUploader("avatars/admin");
-export const uploadProductImage = createUploader("products");
